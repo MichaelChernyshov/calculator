4b4449d855a5581f24c377d72f60900f4dde88b9
MichaelChernyshov <chernyshov.michaele@icloud.com>
2023-02-03 14:46:57 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+function calculateForUsers(name, currentAge, maxAge, estimatedAmount) {
+    name = prompt("What's yoyr name?");
+    currentAge = prompt("Your age");
+    maxAge = 80;
+    estimatedAmount = prompt("How much potatoes you eat weekly?");
+
+    let letYearsInWeeks = (maxAge - currentAge) * 365 / 7;
+    let eatedPopatoes = letYearsInWeeks * estimatedAmount;
+    let eatedPopatoesRounded = Math.round(eatedPopatoes)
+    let result = `${name} has ${currentAge} years and will eat ${eatedPopatoesRounded} potatoes untill age of ${maxAge}`
+    alert(result)
+}
+
+calculateForUsers();
